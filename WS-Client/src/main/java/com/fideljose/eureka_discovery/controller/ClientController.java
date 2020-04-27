61e65b51d43bcb6735abd80bda3fb3532871492e
Fidel Jose <fideljose1708@hotmail.com>
2020-04-26 20:41:17 -0500

    Config load balance to Client project

--- WS-Client/src/main/java/com/fideljose/eureka_discovery/controller/ClientController.java
+++ WS-Client/src/main/java/com/fideljose/eureka_discovery/controller/ClientController.java
@@ -1,5 +1,7 @@
 package com.fideljose.eureka_discovery.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,9 +10,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/client")
 public class ClientController {
 
+	@Autowired
+	private Environment env;
+	
 	@GetMapping("/status/check")
 	public String index() {
-		return "Client Up, services Working!!!";
+		return "Client Up, services Working!!! on port number:" 
+				+ env.getProperty("local.server.port");
 	}
 	
 }
