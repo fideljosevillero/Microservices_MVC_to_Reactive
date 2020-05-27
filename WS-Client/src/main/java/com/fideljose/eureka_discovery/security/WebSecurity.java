d8b00c199e00727d533abf2606f9bde6ed29b41e
Fidel Jose <fideljose1708@hotmail.com>
2020-05-27 07:38:22 -0500

    Add gateway IP from .properties file

--- WS-Client/src/main/java/com/fideljose/eureka_discovery/security/WebSecurity.java
+++ WS-Client/src/main/java/com/fideljose/eureka_discovery/security/WebSecurity.java
@@ -1,6 +1,7 @@
 package com.fideljose.eureka_discovery.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -8,10 +9,17 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 @EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter {
+	
+	protected Environment env;
+	
+	public WebSecurity(Environment env) {
+		this.env = env;
+	}
+	
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable();
-		http.authorizeRequests().antMatchers("/**").hasIpAddress("192.168.1.6");
+		http.authorizeRequests().antMatchers("/**").hasIpAddress(env.getProperty("gateway.ip"));
 		http.headers().frameOptions().disable();
 	}
 	
